4e30f9b21b96deeca3a05c3ca4c21e9adb7ff530
IsakNaslundBh <isak.naslund@burohappold.com>
2025-02-05 15:37:10 +0100

    Add HealthAndSafety

---
+++ dKoP_oM/HealthAndSafety/HealthAndSafety.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BH.oM.dKoP.HealthAndSafety
+{
+    public class HealthAndSafety : IdKopObject
+    {
+        /***************************************************/
+        /**** Properties                                ****/
+        /***************************************************/
+
+        public virtual string SiteRelatedSignificantResidualRisks { get; set; } = "";
+
+        public virtual string COSSHCertification { get; set; } = "";
+
+        public virtual bool SafetyDataSheets { get; set; }
+
+        public virtual string VOCTestAndEmissionsCertificate { get; set; } = "";
+
+        public virtual string ProductSignificantResidualRisk { get; set; } = "";
+
+        public virtual string StructuralCertification { get; set; } = "";
+
+        /***************************************************/
+    }
+}
