7a3ecf5bc9523db64901007614f8d921badfb383
IsakNaslundBh <isak.naslund@burohappold.com>
2025-02-05 15:33:36 +0100

    Add QualityAndCertification

---
+++ dKoP_oM/QualityAndCertification/QualityAndCertification.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Principal;
+using System.Text;
+
+namespace BH.oM.dKoP.QualityAndCertification
+{
+    public class QualityAndCertification : IdKopObject
+    {
+        /***************************************************/
+        /**** Properties                                ****/
+        /***************************************************/
+
+        public virtual string DLUHCPrincipalsDocumentReferenceNumber { get; set; } = "";
+
+        public virtual string DLUHCPrincipalsDocumentReferenceVersion { get; set; } = "";
+
+        public virtual bool CompliantWithDLUHCPrincipalsDocument { get; set; }
+
+        public virtual string AssuranceSchemeName { get; set; } = "";
+
+        public virtual string AssuranceCertification { get; set; } = "";
+
+        /***************************************************/
+    }
+}
