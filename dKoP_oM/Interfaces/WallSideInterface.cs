68881b9c7b5fcbf880d7ebf9af3bd0db59a92658
IsakNaslundBh <isak.naslund@burohappold.com>
2025-02-21 10:34:54 +0100

    Fix interface classes having mixed up names

--- dKoP_oM/Interfaces/WallSideInterface.cs
+++ dKoP_oM/Interfaces/WallSideInterface.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace BH.oM.dKoP
 {
-    public class FloorAndRoofSideInterface : IdKopObject
+    public class WallSideInterface : IdKopObject
     {
         /***************************************************/
         /**** Properties                                ****/
